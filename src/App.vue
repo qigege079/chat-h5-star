f6823c1ec5fe14aaf3494438c7478742339a21fa
panqi <panqi@cdzgph.com>
2025-12-19 14:57:26 +0800

    Initial commit: 小星陪伴聊天 H5 项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Chat from './components/Chat.vue'
+</script>
+
+<template>
+  <div class="h-full w-full bg-base-200">
+    <Chat />
+  </div>
+</template>
+
+<style>
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
